31adffeb9d3b9b31b2e2e0e7c0b20765bafa0671
Weaver Goldman <we.goldm@gmail.com>
2026-02-21 19:31:10 -0500

    Add rendered guitar

--- src/components/guitar.ts
+++ src/components/guitar.ts
@@ -1,12 +1,15 @@
-import guitarOutline from "../assets/guitar-outline.svg"
+// import guitarOutline from "../assets/guitar-outline.svg"
+import guitar from "../assets/Guitar.svg"
 
 export const addGuitarOutline = (container: HTMLElement) => {
     const img = document.createElement("img")
-    img.src = guitarOutline
+    img.src = guitar
     img.alt = "Guitar Outline"
+    img.style.width = "80%"
+    img.style.height = "auto"
     img.style.position = "absolute"
     img.style.top = "50%"
-    img.style.left = "60%"
+    img.style.left = "55%"
     img.style.transform = "translate(-50%, -50%)"
     container.appendChild(img)
 }
